7cb65f636e74e3fecd3a75b6acc148229695eac9
Phelipe Freire Pato <pheprogrammer@gmail.com>
2024-03-05 20:07:14 -0300

    Add inversion dependence exemple

---
+++ interface/inversao dependencia/exemplo_inversao_dependencia.py
@@ -0,0 +1,45 @@
+import json
+
+# Isso é uma interface que está definindo assinaturas usando o principio de segregação de interface
+class Animal:
+    def call(self, falar: str) -> str:
+        raise NotImplementedError()
+    
+# Isso é um adaptador
+class Gato(Animal):
+    def call(self, falar: str) -> str:
+        return " ".join(["miau" for _ in falar.split(" ")])
+
+# Isso é um adaptador
+class Chachorro(Animal):
+    def call(self, falar: str) -> str:
+        return " ".join(["auau" for _ in falar.split(" ")])
+    
+# Isso é um adaptador
+class Pinto(Animal):
+    def call(self, falar: str) -> str:
+        return " ".join(["piupiu" for _ in falar.split(" ")])
+    
+# Isso é um adaptador
+class Pato(Animal):
+    def call(self, falar: str) -> str:
+        return " ".join(["quack" for _ in falar.split(" ")])
+    
+# Isso é um fluxo recebendo uma interface como parametro usando os principios de:
+    # Inversão de dependencia
+    # Substituição de Liskov
+def fluxo_de_falar(animal: Animal):
+    print(animal.call("nicole maneira barbie gostoso"))
+
+
+injecao = {
+    "gato": Gato(),
+    "chachorro": Chachorro(),
+    "pinto": Pinto(),
+    "pato": Pato(),
+}
+
+with open("./bridge.json") as j:
+    dict_json = json.load(j)
+    animal = injecao[dict_json["animal"]]
+    fluxo_de_falar(animal)
